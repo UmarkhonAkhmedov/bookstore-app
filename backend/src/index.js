d83149ccbd448dd0e92113f96300d32a79efa0c4
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2025-01-21 20:18:53 +0500

    Changing

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,12 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ua-bookstore.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.use("/api/books", bookRoutes);
 app.use("/api/orders", orderRoutes);
